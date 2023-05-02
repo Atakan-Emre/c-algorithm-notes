88328a17432560d8277acb024f73ccab90a16947
atakanEmre <atakan21@outlook.com>
2023-05-02 15:35:19 +0300

    azalan fonksiyon eklendi

--- ders-notlari/3.1_Fonksiyonlar.c
+++ ders-notlari/3.1_Fonksiyonlar.c
@@ -292,3 +292,43 @@ int main() {
 }
 
 */
+
+#include <stdio.h>
+#define SIZE 10
+
+// program çalışmasını başlatan main fonksiyonu
+int main(void)
+{
+    // a dizisi tanımlanıp başlangıç değerleri atanıyor
+    int a[SIZE] = {2, 6, 4, 8, 10, 12, 89, 68, 45, 37};
+
+    puts("Orijinal siralama:");
+    
+    // dizinin orijinal hali yazdırılıyor
+    for (size_t i = 0; i < SIZE; ++i) {
+        printf("%4d", a[i]);
+    }
+    
+    // bubble sort (kabarcık sıralama) algoritması
+    // her geçişte karşılaştırma sayısını kontrol eden dış döngü
+    for (unsigned int pass = 1; pass < SIZE; ++pass) {
+        // komşu elemanları karşılaştırıp birbirleriyle yer değiştirme işlemi
+        // ilk elemanın ikinci elemandan küçük olması durumunda yer değiştirme
+        for (size_t i = 0; i < SIZE - 1; ++i) {
+            if (a[i] < a[i + 1]) {
+                int hold = a[i];
+                a[i] = a[i + 1];
+                a[i + 1] = hold;
+            }
+        }
+    }
+    
+    puts("\nAzalan sirayla veriler:");
+    
+    // sıralanmış dizi yazdırılıyor
+    for (size_t i = 0; i < SIZE; ++i) {
+        printf("%4d", a[i]);
+    }
+    
+    puts("");
+}
