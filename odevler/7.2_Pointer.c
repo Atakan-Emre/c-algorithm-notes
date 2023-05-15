5db8e2efb0f5658226ce706c04433781fcae4065
atakanEmre <atakan21@outlook.com>
2023-05-15 21:33:07 +0300

    7.2 pointer

---
+++ odevler/7.2_Pointer.c
@@ -0,0 +1,17 @@
+//Using the character and string conversion specifiers
+#include <stdio.h>
+
+int main(void)
+{
+
+char character = 'A'; // initialize char
+printf("%c\n",character); 
+
+printf ("%s\n","This is a string");
+
+char string[] = "This is a string"; // initialize char array
+printf("%s\n",string);
+
+char *stringPtr = "This is also a string"; //char pointer
+printf("%s\n", stringPtr);
+}
