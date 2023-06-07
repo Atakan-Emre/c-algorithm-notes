5ea06c663c91f684984882ec11173eff659b21df
Atakan Emre <satakanemre@gmail.com>
2023-06-07 23:36:43 +0300

    random 1

---
+++ ders-notlari/Random.c
@@ -0,0 +1,18 @@
+/*
+#include<stdio.h>
+
+unsigned long long int factorial(int n) {
+   if(n == 0)
+      return 1;
+   else
+      return n * factorial(n - 1);
+}
+
+int main() {
+   int n;
+   printf("Enter a positive integer: ");
+   scanf("%d", &n);
+   printf("Factorial of %d = %llu", n, factorial(n));
+   return 0;
+}
+*/
