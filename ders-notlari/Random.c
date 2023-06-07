9ae4e11d22995346be4c8d57b80b3aaf4f700c15
Atakan Emre <satakanemre@gmail.com>
2023-06-07 23:37:26 +0300

    random3

--- ders-notlari/Random.c
+++ ders-notlari/Random.c
@@ -40,3 +40,35 @@ int main() {
    return 0;
 }
 */
+/*
+#include<stdio.h>
+
+#define SIZE 5
+
+void reverse(int arr[], int start, int end) {
+   int temp;
+   while(start < end) {
+      temp = arr[start];   
+      arr[start] = arr[end];
+      arr[end] = temp;
+      start++;
+      end--;
+   }   
+}    
+
+void printArray(int arr[], int size) {
+   int i;
+   for(i = 0; i < size; i++)
+      printf("%d ", arr[i]);
+   printf("\n");
+} 
+
+int main() {
+   int arr[SIZE] = {1, 2, 3, 4, 5};
+   printArray(arr, SIZE);
+   reverse(arr, 0, SIZE - 1);
+   printf("Reversed array is \n");
+   printArray(arr, SIZE);
+   return 0;
+}
+*/
