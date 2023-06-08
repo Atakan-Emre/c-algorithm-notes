50fb83f336cbe88ffad6780dc8c90792b8a36b46
Atakan Emre <satakanemre@gmail.com>
2023-06-08 21:58:55 +0300

    rast3

--- ders-notlari/Random.c
+++ ders-notlari/Random.c
@@ -179,3 +179,32 @@ int main() {
 }
 
 */
+
+/*
+#include <stdio.h>
+int isPerfect(int num) {
+    int sum = 1;
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) {
+            if (i * i != num)
+                sum = sum + i + num / i;
+            else
+                sum = sum + i;
+        }
+    }
+    if (sum == num && num != 1)
+        return 1;
+    return 0;
+}
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if (isPerfect(num))
+        printf("%d is a perfect number", num);
+    else
+        printf("%d is not a perfect number", num);
+    return 0;
+}
+
+*/
