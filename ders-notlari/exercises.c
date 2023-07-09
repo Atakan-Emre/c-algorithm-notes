9b48032d4d9aa06a035a954d8ec8f7b7be8fb457
Atakan Emre <satakanemre@gmail.com>
2023-07-09 22:46:32 +0300

    ex1

---
+++ ders-notlari/exercises.c
@@ -0,0 +1,29 @@
+/*
+#include <stdio.h>
+
+int main() {
+    int number;
+    printf("Enter an integer: ");
+    scanf("%d", &number);
+    
+    FILE *fptr;
+    fptr = fopen("file1.txt", "w");
+    
+    if (fptr == NULL) {
+        printf("Error!");
+        return 1;
+    }
+
+    fprintf(fptr, "%d", number);
+    fclose(fptr);
+
+    fptr = fopen("file1.txt", "r");
+    fscanf(fptr, "%d", &number);
+
+    printf("Value of n=%d", number);
+    fclose(fptr);
+
+    return 0;
+}
+
+*/
