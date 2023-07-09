df4636646d58ed465286c065d6590f3c8dcff494
Atakan Emre <satakanemre@gmail.com>
2023-07-09 22:46:53 +0300

    ex2

--- ders-notlari/exercises.c
+++ ders-notlari/exercises.c
@@ -27,3 +27,40 @@ int main() {
 }
 
 */
+
+/*
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char word[100];
+    printf("Enter a word: ");
+    scanf("%s", word);
+    
+    FILE *fptr;
+    fptr = fopen("file2.txt", "w");
+    
+    if (fptr == NULL) {
+        printf("Error!");
+        return 1;
+    }
+
+    fprintf(fptr, "%s", word);
+    fclose(fptr);
+
+    fptr = fopen("file2.txt", "r");
+    char ch;
+    int index = strlen(word);
+    
+    printf("Reversed word is: ");
+    while (index >= 0) {
+        ch = fgetc(fptr);
+        printf("%c", word[index]);
+        index = index - 1;
+    }
+    fclose(fptr);
+
+    return 0;
+}
+
+*/
