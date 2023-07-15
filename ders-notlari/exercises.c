f192ea02c550a31c4eeb24073347ef75d7f8bdb7
Atakan Emre <satakanemre@gmail.com>
2023-07-15 22:17:58 +0300

    exc2

--- ders-notlari/exercises.c
+++ ders-notlari/exercises.c
@@ -880,3 +880,31 @@ int main() {
 }
 
 */
+
+/*
+#include <stdio.h>
+#include <string.h>
+
+int isPalindrome(char str[]) {
+    int l = 0;
+    int h = strlen(str) - 1;
+    while (h > l) {
+        if (str[l++] != str[h--]) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main() {
+    char str[100];
+    printf("Enter a string: ");
+    scanf("%s", str);
+    if (isPalindrome(str))
+        printf("Yes\n");
+    else
+        printf("No\n");
+    return 0;
+}
+
+*/
