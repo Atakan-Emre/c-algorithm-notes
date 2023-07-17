66032f0ea56ead6da9bb9495300682c1f1e3815e
Atakan Emre <satakanemre@gmail.com>
2023-07-17 23:29:43 +0300

    ex2

--- ders-notlari/exercises.c
+++ ders-notlari/exercises.c
@@ -1083,3 +1083,45 @@ void quicksort(int number[25],int first,int last){
 }
 
 */
+
+/*
+#include <stdio.h>
+#define INF 99999
+#define N 4
+
+void printSolution(int dist[][N]);
+
+void floydWarshall (int graph[][N]) {
+    int dist[N][N], i, j, k;
+
+    for (i = 0; i < N; i++)
+        for (j = 0; j < N; j++)
+            dist[i][j] = graph[i][j];
+
+    for (k = 0; k < N; k++) {
+        for (i = 0; i < N; i++) {
+            for (j = 0; j < N; j++) {
+                if (dist[i][k] + dist[k][j] < dist[i][j])
+                    dist[i][j] = dist[i][k] + dist[k][j];
+            }
+        }
+    }
+
+    printSolution(dist);
+}
+
+void printSolution(int dist[][N]) {
+    printf ("Following matrix shows the shortest distances"
+            " between every pair of vertices \n");
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (dist[i][j] == INF)
+                printf("%7s", "INF");
+            else
+                printf ("%7d", dist[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+*/
